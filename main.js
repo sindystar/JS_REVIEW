47c8deb030835f86e3e6821108597e1876f223b9
sindystar <hasungah@gmail.com>
2023-03-14 22:31:15 +0900

    연산자

--- main.js
+++ main.js
@@ -1,68 +1,21 @@
 /* 
-원시형 자료(primitive type) 특정값이 메모리에 바로 저장. 값만 저장 된것 
-1. 문자열(string)
-2. tntwk(number)
-3. 불린형, 참거짓형(boolean) = true, false
-4. underfined - 변수를 만들고 할당하지 않으면 underfined으로 저장됨, 에러상황
-5. null(object) - 갑싱 비어있긴 하지만 일부러, 명시적으로 값을 비워둔경우 
-- 메모리를 만들어야 하는데 값은 지금 없어서 넣지 않을때 일부러 비워둠 
+연산자 : 특정값을 도출 하기 위해 연산을 처리하는 식별자 
 
+산술 연산자 : ++, -- 연속해서 1씩 더하거나 빼는 것 **
++%
+대입 연산자 : =등호가 아님 
+a = b a와 ㅠ가ㅣ 같다는 뜻이 아님, a에 다가 bfmf 대입 하라는 뜻
+a == a와 b가 같음을 의미
+a === b a와 b가 같은데 어떤 수준까지? 자료형까지 완벽하게 같음을 의미
 
-참조형 자료 (reference type)값의 참조값, 주소값만 메모리에 저장 되는것
-5. null(object) - 값이 비어있긴 하지만 일부러 , 명시적으로 값을 비워둔 경우 
-- 메모리는 만들어야 하는데 값은 지금 없어서 넣지 않을때 일부러 비워둠 
-1. 배열(array) = 여러개의 값들을 그룹으로 묶어서 저장된 형태 
-(각 값들은 순서값으로 탐색 가능하다)
-2. 객체(object) - 여러개의 값들을 그룹으로 묶어서 저장된 형태 
-(각 값들 마다 고유의 key값을 넣어서 저장하고 탐색 할때도 key값으로 탐색)
+비교 연산자 : < > <= >= 불린 값을 반환 한다
+
+전위 연산자 : ++num 변수 초기 값에서 연산을 먼저 하고 그뒤에 연산된 값을 활용한다 
+후위 연산자 : num++ 변수 초기 값을 먼 저 활용하고 그뒤에 값을 연산 처리 한다 
+
 */
 
-
-
-// let num1 = "2"; //문자 
-// let num2 = 2; //숫자 
-// let isOk = true; //boolean
-// let error;
-// let isBlank = null
-
-// console.log(typeof num1);
-// console.log(typeof num2);
-// console.log(typeof isOk);
-// console.log(typeof error);
-// console.log(typeof isBlank);
-
-// 같은 성격의 일련의 데이터들은 그룹 형태로 저장하면 -array
-let colors = ["red", "yellow", "brown","blue"];
-console.log(colors[0]);
-console.log(colors[3]);
-
-//let items = [{},{},{},{}];
-
-let student1 = {
-  name: "홍길동",
-  age : 20,
-  address: "Seoul",
-  isMale:true
-};
-//성격이 다른 데이터들의 prop(erty)에 담아서 구조적으로 저장
-console.log(student1.name);
-
-//형변환 : 연산 되는 숫사 num2를 강제로 문자열로 변형 시켜서 두개의 문자로 인식하고 서로 이어 주는것 
-
-let num1 = "2";
-let num2 = 3;
-let num3 = 5;
-let result = num1 + num2;
-let result2 = num2 + num1;
+let num1 = 3;
+let result = ++num1;
+// num1 = num1 + 1
 console.log(result);
-console.log(result2);
-
-let result3 = num1 + num2 + num3;  //235
-let result4 = num3 + num2 + num1; //82
-console.log(result3);
-console.log(result4);
-
-
-/*
-
-*/
